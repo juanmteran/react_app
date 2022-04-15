45b7fc70add2fcf7b8b5a948d6157d0450d99a9d
juanmteran <juanmteranflores@outlook.com>
2022-04-15 17:30:46 -0500

    WEEK1COMPLETE

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,21 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import { Menu } from './components/MenuComponent';
+
+import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import { Component } from 'react';
 
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color='primary'>
-        <div className='container'>
-          <NavbarBrand href='/'>Ristorante con fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu/>
-    </div>
-  );
+  render() {
+          return (
+            <div className="App">
+              <Navbar dark color='primary'>
+                <div className='container'>
+                  <NavbarBrand href='/'>Ristorante con fusion</NavbarBrand>
+                </div>
+              </Navbar>
+              <Menu dishes={this.state.dishes}/>
+            </div>
+          );
+  }
 }
 
 export default App;
