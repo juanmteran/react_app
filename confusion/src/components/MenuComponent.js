f41539c6dab37bfe617f32e996ab1ce65ce8c81e
juanmteran <juanmteranflores@outlook.com>
2022-04-19 10:21:54 -0500

    functionalcomponentW2

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React/*, { Component }*/ from "react";
 import {
   Card,
   CardImg,
@@ -6,40 +6,53 @@ import {
   CardTitle,
 } from "reactstrap";
 
-class Menu extends Component {
+/*class Menu extends Component {
   // eslint-disable-next-line no-useless-constructor
   constructor(props) {
     super(props);
     };
 
 
+  render()*/ 
 
-  render() {
+    function RenderMenuItem({dish, onClick}) {
+      return(
+        <Card onClick={() => onClick(dish.id)}>
+        <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <CardImgOverlay>
+          <CardTitle> {dish.name}</CardTitle>
+        </CardImgOverlay>
+      </Card>
+      );
+    } 
 
+    const Menu = (props) =>{
+      const menu = props.dishes.map((dish) => {
+        return (
+          <div key={dish.id} className="col-12 col-md-5 m-1">
+            <RenderMenuItem dish={dish} onClick={props.onClick}/>  
+          </div>
+        );
+      });
 
-    const menu = this.props.dishes.map((dish) => {
       return (
-        <div key={dish.id} className="col-12 col-md-5 m-1">
-          <Card onClick={() => this.props.onClick(dish.id)}>
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
-            <CardImgOverlay>
-              <CardTitle> {dish.name}</CardTitle>
-            </CardImgOverlay>
-          </Card>
+        <div className="container">
+          <div className="row">{menu}</div>
         </div>
       );
-    });
+    }
 
-    return (
-      <div className="container">
-        <div className="row">{menu}</div>
-      </div>
-    );
-  }
 
-  componentDidMount() {
+
+
+    
+
+
+  
+
+  /*componentDidMount() {
     console.log("Menu component componentDidMounbt is invoked");
   }
-}
+//}*/
 
 export default Menu;
